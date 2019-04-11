ac56ecd3c8b3e62102f473c8ba75d239c2c3f635
Bartosz Krajka <krajka.bartosz@gmail.com>
2019-04-11 18:02:06 +0200

    Add triangulating test case for SettleWithRoad
    Commented out for the moment - requires `EndTurn` controller
    to be implemented

--- spec/interactors/settle_with_road_spec.rb
+++ spec/interactors/settle_with_road_spec.rb
@@ -8,50 +8,107 @@ describe SettleWithRoad do
 
   subject(:call) { game.handle(interactor) }
 
-  context 'with valid data and state' do
-    before(:each) { game.handle(@setup_game_interactor) }
+  context 'with valid data' do
+    context 'when on clear state' do
+      before(:each) { game.handle(@setup_game_interactor) }
 
-    it_behaves_like 'mutating interaction'
+      it_behaves_like 'mutating interaction'
 
-    it 'returns success' do
-      expect(call.success?).to be(true)
+      it 'returns success' do
+        expect(call.success?).to be(true)
+      end
+
+      it 'settles one spot' do
+        call
+        expect(game.settlements.length).to eq(1)
+      end
+
+      it 'settles correct spot' do
+        player = game.current_player
+        call
+        expect(game.settlements.first).to eq(Settlement.new(spot_index: 1, owner: player))
+      end
+
+      it 'adds resources to current player' do
+        current_player_name = game.current_player.name
+        call
+        previous_player = game.find_player_by_name(current_player_name)
+        expected = Resources.new(
+          brick: 2,
+          lumber: 1,
+          wool: 0,
+          grain: 0,
+          ore: 0
+        )
+        expect(previous_player.resources).to eq(expected)
+      end
+
+      it 'builds one road' do
+        call
+        expect(game.roads.length).to eq(1)
+      end
+
+      it 'builds correct road' do
+        player = game.current_player
+        call
+        expect(game.roads.first).to eq(Road.new(from: 1, to: 2, owner: player))
+      end
     end
 
-    it 'settles one spot' do
-      call
-      expect(game.settlements.length).to eq(1)
-    end
-
-    it 'settles correct spot' do
-      player = game.current_player
-      call
-      expect(game.settlements.first).to eq(Settlement.new(spot_index: 1, owner: player))
-    end
-
-    it 'adds resources to current player' do
-      current_player_name = game.current_player.name
-      call
-      previous_player = game.find_player_by_name(current_player_name)
-      expected = Resources.new(
-        brick: 2,
-        lumber: 1,
-        wool: 0,
-        grain: 0,
-        ore: 0
-      )
-      expect(previous_player.resources).to eq(expected)
-    end
-
-    it 'builds one road' do
-      call
-      expect(game.roads.length).to eq(1)
-    end
-
-    it 'builds correct road' do
-      player = game.current_player
-      call
-      expect(game.roads.first).to eq(Road.new(from: 1, to: 2, owner: player))
-    end
+    # context 'when on not clear state' do
+    #   before(:each) do
+    #     game.handle(@setup_game_interactor)
+    #     game.handle(SettleWithRoad.new(settlement_spot: 3, road_extension_spot: 4))
+    #   end
+    #
+    #   it_behaves_like 'mutating interaction'
+    #
+    #   it 'returns success' do
+    #     expect(call.success?).to be(true)
+    #   end
+    #
+    #   it 'settles one spot' do
+    #     old_length = game.settlements.length
+    #     call
+    #     expect(game.settlements.length).to eq(old_length + 1)
+    #   end
+    #
+    #   it 'settles correct spot' do
+    #     old_settlements = game.settlements
+    #     player = game.current_player
+    #     call
+    #     new_settlement = (game.settlements - old_settlements).first
+    #     expect(new_settlement).to eq(Settlement.new(spot_index: 1, owner: player))
+    #   end
+    #
+    #   it 'adds resources to current player' do
+    #     current_player_name = game.current_player.name
+    #     call
+    #     previous_player = game.find_player_by_name(current_player_name)
+    #     expected = Resources.new(
+    #       brick: 2,
+    #       lumber: 1,
+    #       wool: 0,
+    #       grain: 0,
+    #       ore: 0
+    #     )
+    #     expect(previous_player.resources).to eq(expected)
+    #   end
+    #
+    #   it 'builds one road' do
+    #     old_length = game.roads.length
+    #     call
+    #     expect(game.roads.length).to eq(old_length + 1)
+    #   end
+    #
+    #   it 'builds correct road' do
+    #     old_roads = game.roads
+    #     player = game.current_player
+    #     call
+    #     new_road = (old_roads - game.roads).first
+    #     expect(new_road).to eq(Road.new(from: 1, to: 2, owner: player))
+    #   end
+    # end
   end
 
   context 'when game not set up' do
